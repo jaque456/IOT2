0e640d3ffdf502ea405fe7f64ea9d0ab4ead6412
jaque456 <56366968+jaque456@users.noreply.github.com>
2020-02-20 15:56:55 -0600

    Add files via upload

---
+++ Elefantes.py
@@ -0,0 +1,9 @@
+class Elefantes():
+
+    def cancion(self):
+        for i in range(1,101):
+            print(i," Elefante se columpiaban sobre la tela de una arana como veia que resistia fueron a llamar otro elefante")
+b=Elefantes()
+b.cancion()
+    
+	
